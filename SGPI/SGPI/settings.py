1ace365a8382455f1476cd02f2603d5bf7f7b4e5
Cesar002 <soul.unleashed13@gmail.com>
2018-03-13 12:56:05 -0600

    commit de prueba

--- SGPI/SGPI/settings.py
+++ SGPI/SGPI/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sgpi',
         'USER': 'postgres',
-        'PASSWORD': '4815162342',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
